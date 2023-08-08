aca03f86c9cdbc862601f494bd07d9fd03f837bd
Khalid Kamil <khalidkamil28@gmail.com>
2023-08-08 16:28:52 +0100

    Building custom transitions using ViewModifier

--- Animations/ContentView.swift
+++ Animations/ContentView.swift
@@ -1,4 +1,4 @@
-// Showing and hiding views with transitions
+// Building custom transitions using ViewModifier
 
 import SwiftUI
 
@@ -6,16 +6,21 @@ struct ContentView: View {
   @State private var isShowingRed = false
 
   var body: some View {
-    VStack {
-      Button("Tap Me") {
-        isShowingRed.toggle()
-      }
+    ZStack {
+      Rectangle()
+        .fill(.blue)
+        .frame(width: 200, height: 200)
 
       if isShowingRed {
         Rectangle()
           .fill(.red)
           .frame(width: 200, height: 200)
-          .transition(.asymmetric(insertion: .scale, removal: .opacity))
+          .transition(.pivot)
+      }
+    }
+    .onTapGesture {
+      withAnimation {
+        isShowingRed.toggle()
       }
     }
   }
@@ -24,5 +29,24 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
   static var previews: some View {
     ContentView()
+  }
+}
+
+struct CornerRotateModifier: ViewModifier {
+  let amount: Double
+  let anchor: UnitPoint
+
+  func body(content: Content) -> some View {
+    content
+      .rotationEffect(.degrees(amount), anchor: anchor)
+      .clipped()
+  }
+}
+
+extension AnyTransition {
+  static var pivot: AnyTransition {
+    .modifier(
+      active: CornerRotateModifier(amount: -90, anchor: .topLeading),
+      identity: CornerRotateModifier(amount: 0, anchor: .topLeading))
   }
 }

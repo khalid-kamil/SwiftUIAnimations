e637af07f544d5c4627516f27ad00eaf24a19f1b
Khalid Kamil <khalidkamil28@gmail.com>
2023-08-08 11:54:25 +0100

    Implicit Animations

--- Animations/ContentView.swift
+++ Animations/ContentView.swift
@@ -8,19 +8,44 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+  @State private var animationAmount = 1.0
+
+  var body: some View {
+    VStack {
+      Button("Tap me") {
+//        animationAmount += 1
+      }
+      .padding(50)
+      .background(.red)
+      .foregroundColor(.white)
+      .clipShape(Circle())
+//      .scaleEffect(animationAmount)
+//      .blur(radius: (animationAmount - 1) * 3)
+      .overlay(
+        Circle()
+          .stroke(.red)
+          .scaleEffect(animationAmount)
+          .opacity(2 - animationAmount)
+          .animation(
+            .easeInOut(duration: 1)
+            .repeatForever(autoreverses: false), value: animationAmount
+          )
+      )
+      .onAppear {
+        animationAmount = 2
+      }
+      //      .animation(.default, value: animationAmount)
+      //      .animation(.interpolatingSpring(stiffness: 50, damping: 5), value: animationAmount)
+      //      .animation(.easeInOut(duration: 2), value: animationAmount)
+      //      .animation(.easeInOut(duration: 2).delay(1), value: animationAmount)
+      //      .animation(.easeInOut(duration: 1).repeatCount(3, autoreverses: true), value: animationAmount)
     }
+    .padding()
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }

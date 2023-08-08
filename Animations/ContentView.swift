50e27582b61090b73b82088b1151c18b11417a1a
Khalid Kamil <khalidkamil28@gmail.com>
2023-08-08 12:10:33 +0100

    Animating bindings - example 1

--- Animations/ContentView.swift
+++ Animations/ContentView.swift
@@ -11,34 +11,23 @@ struct ContentView: View {
   @State private var animationAmount = 1.0
 
   var body: some View {
-    VStack {
+    print(animationAmount)
+
+    return VStack {
+      Spacer()
+
       Button("Tap me") {
-//        animationAmount += 1
+        animationAmount += 1
       }
       .padding(50)
       .background(.red)
       .foregroundColor(.white)
       .clipShape(Circle())
-//      .scaleEffect(animationAmount)
-//      .blur(radius: (animationAmount - 1) * 3)
-      .overlay(
-        Circle()
-          .stroke(.red)
-          .scaleEffect(animationAmount)
-          .opacity(2 - animationAmount)
-          .animation(
-            .easeInOut(duration: 1)
-            .repeatForever(autoreverses: false), value: animationAmount
-          )
-      )
-      .onAppear {
-        animationAmount = 2
-      }
-      //      .animation(.default, value: animationAmount)
-      //      .animation(.interpolatingSpring(stiffness: 50, damping: 5), value: animationAmount)
-      //      .animation(.easeInOut(duration: 2), value: animationAmount)
-      //      .animation(.easeInOut(duration: 2).delay(1), value: animationAmount)
-      //      .animation(.easeInOut(duration: 1).repeatCount(3, autoreverses: true), value: animationAmount)
+      .scaleEffect(animationAmount)
+
+      Spacer()
+
+      Stepper("Scale amount", value: $animationAmount.animation(), in: 1...10)
     }
     .padding()
   }
